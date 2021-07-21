baa24d60513026eef9694129d5de1df3d89934f5
Borja Candela Salinas <65466303+BorjaCandela@users.noreply.github.com>
2021-07-21 23:04:03 +0200

    Add files via upload

---
+++ Apagado Controlado/Clases/Accion.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace Apagado_Controlado.Clases
+{
+    class Accion
+    {
+        public static void f_apagar(int days, int hours, int mins)
+        {
+
+            int total;
+
+            if (days > 0)
+            {
+                days = days * (24 * 3600); // 24h * 60m * 60s * 1 = 86400s = 1d
+            }
+
+            if (hours > 0)
+            {
+                hours = hours * 3600;
+            }
+
+            if (mins > 0)
+            {
+                mins = mins * 60;
+            }
+
+            total = days + hours + mins;
+            // MessageBox.Show(total.ToString());
+
+
+            if (total == 0)
+            {
+                // Initializes the variables to pass to the MessageBox.Show method.
+                string message = "¿Seguro que quieres apagar el ordenador sin cuenta atras?";
+                string caption = "Confirma por favor";
+
+                // Displays the MessageBox.
+                if (System.Windows.MessageBox.Show(message, caption, MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    var exec = new ProcessStartInfo("shutdown", "/s /f /t " + total);
+                    exec.CreateNoWindow = true;
+                    exec.UseShellExecute = false;
+                    Process.Start(exec);
+                }
+            }
+
+            if (total > 0)
+            {
+                var exec = new ProcessStartInfo("shutdown", "/s /f /t " + total);
+                exec.CreateNoWindow = true;
+                exec.UseShellExecute = false;
+                Process.Start(exec);
+            }
+
+
+        }
+
+        public static void f_anular()
+        {
+            var psi = new ProcessStartInfo("shutdown", "/a");
+            psi.CreateNoWindow = true;
+            psi.UseShellExecute = false;
+            Process.Start(psi);
+        }
+
+
+    }
+}
---
+++ Apagado Controlado/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Apagado_Controlado.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Apagado_Controlado
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            double screenWidth = System.Windows.SystemParameters.PrimaryScreenWidth;
+            double screenHeight = System.Windows.SystemParameters.PrimaryScreenHeight;
+            double windowWidth = this.Width;
+            double windowHeight = this.Height;
+            this.Left = (screenWidth / 2) - (windowWidth / 2);
+            this.Top = (screenHeight / 2) - (windowHeight / 2);
+            InitializeComponent();
+        }
+
+        private void buttonAnularClick(object sender, RoutedEventArgs e)
+        {
+            Accion.f_anular();
+        }
+
+        private void buttonApagarClick(object sender, RoutedEventArgs e)
+        {
+            int days = 0;
+            int hours = 0;
+            int minutes = 0;
+
+            try
+            {
+                bool dayParse = Int32.TryParse(textBoxDias.Text, out days);
+                bool hourParse = Int32.TryParse(textBoxHoras.Text, out hours);
+                bool minuteParse = Int32.TryParse(textBoxMinutos.Text, out minutes);
+
+                if (dayParse && hourParse && minuteParse)
+                {
+                    Accion.f_apagar(days, hours, minutes);
+                }
+
+            } catch (FormatException)
+            {
+                MessageBox.Show("Alguno de los valores no es un numero");
+            }
+
+
+
+        }
+    }
+}
